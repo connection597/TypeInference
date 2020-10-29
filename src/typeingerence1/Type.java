e92b2eb51b0a94495aac158805ce935252190ae7
connection579 <wlswn4118@naver.com>
2020-10-30 01:18:44 +0900

    자료형 추론

---
+++ src/typeingerence1/Type.java
@@ -0,0 +1,19 @@
+package typeingerence1;
+
+public class Type {
+
+	public static void main(String[] args) {
+		var i = 10;
+		var j =10.0;
+		var str = "Hello";
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(str);
+		
+		str ="test";
+		//str =3;
+
+	}
+
+}
